0a96aead1e415b1e509787bf3fa99da1d131d812
Siddhant <s1d.official011001011@gmail.com>
2024-02-07 18:07:02 +0000

    Adsad

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+export default function Slug({ params }: { params: { slug: string } }) {
+  const [data, setData] = useState<any | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        `https://roop.gokapturehub.com/check/${params.slug}`
+      );
+      setData(result.data.result.replace("https://gkh-images.s3.amazonaws.com/", "https://d3n1vk7xba6ap0.cloudfront.net/"));
+    };
+    fetchData();  
+  }, []);
+
+  if (!data) {
+    return <div>Please Wait Your Image is in Process...</div>;
+  }
+
+  const downloadImage = async () => {
+    // open in data url in new tab
+    window.open(data, "_blank");
+  }
+
+  // console.log(data.data)
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col space-y-4">
+      <img alt="" src={data} style={{
+        width: '500px',
+        height: 'auto',
+        borderRadius: "1rem"
+      }} />
+      <button onClick={downloadImage} style={{
+        padding: "1rem",
+        backgroundColor: "blue",
+        color: "white",
+        borderRadius: "1rem",
+        fontSize: "1.5rem",
+        cursor: "pointer"
+      }} >Download</button>
+    </div>
+  );
+}
